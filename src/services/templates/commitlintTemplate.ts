bb4337de39fb50fe7070f5aa66dc2c67b5812279
nakobase <nakobase7@gmail.com>
2026-02-03 14:26:02 +0900

    feat: add overwrite option for template synchronization and improve error handling

--- src/services/templates/commitlintTemplate.ts
+++ src/services/templates/commitlintTemplate.ts
@@ -1,12 +1,13 @@
 import { saveFileToLocal } from '../../fileHandler.js';
 import { handleTemplateSync, TemplateFile } from './templateUtils.js';
 
-export const syncCommitlintTemplate = async () => {
+export const syncCommitlintTemplate = async (overwrite: boolean) => {
   const fileHandlers: Record<string, (file: TemplateFile) => void> = {
     'commitlint.config.js': (file) => {
       saveFileToLocal({
         destPath: `./${file.name}`,
         fileData: file.content,
+        overwrite,
       });
     },
   };
--- src/services/templates/githubTemplates.ts
+++ src/services/templates/githubTemplates.ts
@@ -1,24 +1,27 @@
 import { saveFileToLocal } from '../../fileHandler.js';
 import { handleTemplateSync, TemplateFile } from './templateUtils.js';
 
-export const syncGithubTemplates = async () => {
+export const syncGithubTemplates = async (overwrite: boolean) => {
   const fileHandlers: Record<string, (file: TemplateFile) => void> = {
     'pull_request_template.md': (file) => {
       saveFileToLocal({
         destPath: `./.github/${file.name}`,
         fileData: file.content,
+        overwrite,
       });
     },
     'bug_report.md': (file) => {
       saveFileToLocal({
         destPath: `./.github/ISSUE_TEMPLATE/${file.name}`,
         fileData: file.content,
+        overwrite,
       });
     },
     'feature_request.md': (file) => {
       saveFileToLocal({
         destPath: `./.github/ISSUE_TEMPLATE/${file.name}`,
         fileData: file.content,
+        overwrite,
       });
     },
   };
